ec66ef577a8e9b7f4f5d33eec951dadd1fb0aa74
rexam14 <amerigo.mancino@gmail.com>
2020-07-03 13:47:38 +0200

    Card element set up.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello there, World!")
+        // HStack allows to arrange its child views in a horizontal line
+        HStack(spacing: 5) {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            // these modifiers are applied to all the views inside the stack
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        // ZStack allows to overlap its child views on top of each other
+        ZStack() {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                // Card background: the color is passed down from above
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
