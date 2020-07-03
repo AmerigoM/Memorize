1ae2f1c341a64488082ad9d1e99c311175eae9d7
rexam14 <amerigo.mancino@gmail.com>
2020-07-03 12:14:38 +0200

    Initial commit.

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Amerigo Mancino on 03/07/2020.
+//  Copyright © 2020 Amerigo Mancino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello there, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
