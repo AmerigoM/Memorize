436bf1cd40d48ecd478c9e9c1458ef37737f2974
rexam14 <amerigo.mancino@gmail.com>
2020-07-05 13:07:06 +0200

    Fixed card layout content size using GeometryReader.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -26,27 +26,49 @@ struct EmojiMemoryGameView: View {
             // these modifiers are applied to all the views inside the stack
             .padding()
             .foregroundColor(Color.orange)
-            .font(Font.largeTitle)
     }
 }
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
     
+    // main view
     var body: some View {
-        // ZStack allows to overlap its child views on top of each other
-        ZStack() {
-            if card.isFacedUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                // Card background: the color is passed down from above
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+        // GeometryReader is just a view having a geometry argument which
+        // contains some parameters, among which there is the size (the
+        // amount of space that is being offered to us by the container)
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
     }
+    
+    // helper function containing the view body
+    func body(for size: CGSize) -> some View {
+        // ZStack allows to overlap its child views on top of each other
+         ZStack() {
+            if self.card.isFacedUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
+             } else {
+                 // Card background: the color is passed down from above
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+             }
+         }
+        // we set the content size as the minimum between the space offered
+            .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        return min(size.width, size.height) * 0.75
+    }
+    
 }
+
+// MARK: - Content View Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
