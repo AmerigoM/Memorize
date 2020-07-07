7d06e045dcb2c25c5cd835740a03e706e5f697f3
rexam14 <amerigo.mancino@gmail.com>
2020-07-07 09:18:49 +0200

    Defined a custom Pie shape.

---
+++ Memorize/Pie.swift
@@ -0,0 +1,34 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Amerigo Mancino on 07/07/2020.
+//  Copyright © 2020 Amerigo Mancino. All rights reserved.
+//
+
+import SwiftUI
+
+// custom shape of a pie
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    // only function needed when defining a custom shape
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(CGFloat(startAngle.radians)),
+            y: center.y + radius * sin(CGFloat(startAngle.radians))
+        )
+        
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        p.addLine(to: center)
+        
+        return p
+    }
+}
